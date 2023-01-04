07b796c0b8a07c73a7869b639d8351ad86e4997b
Kranti <darkphoton20@gmail.com>
2023-01-04 17:08:38 +0800

    Adding register, login, and verification pages and hbs to render views

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// for rendering html
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // allows nestjs to strip all properties sent in a request
   // that was now allowed via decorators
   app.useGlobalPipes(
@@ -11,6 +15,12 @@ async function bootstrap() {
       whitelist: true, // for security
     }),
   );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
