de9ec80d1190da1a21d6a80c6b4d31d4c40defaa
Michael Cheng <michaelnccu@gmail.com>
2023-03-01 15:07:26 +0800

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'The app is working correctly.';
+    return 'The app is working correctly..';
   }
 }
